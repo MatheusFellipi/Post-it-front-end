ef9b811f35a6021db8e3897d01012a9653615d92
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-27 15:28:32 -0300

    Craindo o componente de criar um nova quest

---
+++ src/components/note/createnewNote.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
+  Select,
+  Stack,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+
+export function CreateNote() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Button leftIcon={<FaPlus />} colorScheme="teal" onClick={onOpen}>
+        Create quest
+      </Button>
+      <Drawer isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader borderBottomWidth="1px">
+            Create a new quest
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Stack spacing="24px">
+              <Box>
+                <FormLabel htmlFor="username">Quest</FormLabel>
+                <Input id="username" placeholder="Please enter user name" />
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="owner">Status</FormLabel>
+                <Select id="owner" defaultValue="segun">
+                  <option value="realize">Realize</option>
+                  <option value="stoping">Stoping</option>
+                  <option value="finished">Finished</option>
+                </Select>
+              </Box>
+
+              <Box>
+                <FormLabel htmlFor="desc">Description</FormLabel>
+                <Textarea id="desc" />
+              </Box>
+            </Stack>
+          </DrawerBody>
+
+          <DrawerFooter borderTopWidth="1px">
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Submit</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
